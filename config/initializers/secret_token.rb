71f303fd2c440e009e26b5c0888c611ca8e56460
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2012-07-21 19:55:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nakedpunch::Application.config.secret_token = '615705d165e35a9502732e3864b551c459c86ec3218e4ab373d7bb7ae0d35ad8fdf022d0eac3a2ef4b9926b949533d6a5d197bc7d4c447a7c145701baeea1f4f'
