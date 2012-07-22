c0518b15f4ae7f46cce59bbcb5d8c9187515a885
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2012-07-22 15:34:44 -0400

    articles table modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120722171925) do
+ActiveRecord::Schema.define(:version => 20120722191928) do
 
   create_table "articles", :force => true do |t|
     t.string   "author"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(:version => 20120722171925) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
+    t.integer  "issue_id"
+    t.string   "status"
   end
 
 end
