a0213033328117fe3eda0ffab07e9a8293504260
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2012-07-30 19:14:19 -0400

    status field removed

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,5 +1,5 @@
 class Slide < ActiveRecord::Base
   has_one :image, :as => :imageable, :dependent => :destroy
   attr_accessible :page_url, :status, :title, :image_attributes
-  validates :title, :page_url, :status, :image, :presence => true
+  validates :title, :page_url, :image, :presence => true
 end
