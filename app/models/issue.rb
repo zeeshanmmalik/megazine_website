d4dbe8167fc15075f542156110960a8a60cfa57c
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2012-07-30 18:23:12 -0400

    issue has_many articles

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,6 @@
 class Issue < ActiveRecord::Base
   has_one :image, :as => :imageable, :dependent => :destroy
+  has_many :articles
   attr_accessible :currency_code, :description, :price, :status, :title, :image_attributes
   validates :title, :description, :status, :image, :presence => true
 end
